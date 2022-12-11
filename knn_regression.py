14cb15aaa4a9da10cc3007f1796bc0fbd7ddb765
bambrah-tk <bambrah-tk>
2022-12-11 16:02:37 +0100

    3 diffrent codes

--- knn_regression.py
+++ knn_regression.py
@@ -166,3 +166,67 @@ optimal_RMSE
 
 ###########################################
 
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+X, y = load_boston(return_X_y=True)
+train_data, test_data, train_target, test_target = train_test_split(X, y, test_size=0.25)
+print(train_data.shape)
+print(test_data.shape)
+print(train_target.shape)
+print(test_target.shape)
+
+
+#After teaching the machine, we need to get the error of our prediction. For that, I'll use metrics library.
+
+from sklearn import metrics
+
+#Now, we will proceed with the first approach.
+#1. Without any external library
+
+#For calculating the Euclidean distance, I'll use external library from scipy. But for teaching the machine, I won't be using.
+
+from scipy.spatial import distance
+
+class kNNRegressor:
+
+    def __init__(self, neighbors):
+        self.train_data = []
+        self.train_target = []
+        self.neighbors = neighbors
+
+    def fit(self, train_data, train_target):
+        self.train_data = train_data
+        self.train_target = train_target
+
+    def predict(self, test_data):
+        test_predicted = []
+        for i in range(len(test_data)):
+            distances = []
+            for j in range(len(self.train_data)):
+                distances.append((distance.euclidean(self.train_data[j], test_data[i]), self.train_target[j]))
+            distances.sort(key = lambda x: x[0])
+            mean = 0
+            for j in range(self.neighbors):
+                mean += distances[j][1]
+            mean /= self.neighbors
+            test_predicted.append(mean)
+        return test_predicted
+
+#Now, I've the class. I'll proceed with our procedure.
+
+neigh = kNNRegressor(5)
+neigh.fit(train_data, train_target)
+test_predicted = neigh.predict(test_data)
+metrics.mean_absolute_error(test_target, test_predicted)
+
+
+#Now, I'll do the same thing with sklearn's KNeighborsRegressor.
+#2. sklearn's KNeighborsRegressor
+
+from sklearn.neighbors import KNeighborsRegressor
+neigh = KNeighborsRegressor(n_neighbors=5)
+neigh.fit(train_data, train_target)
+test_predicted = neigh.predict(test_data)
+metrics.mean_absolute_error(test_target, test_predicted)
+##########
